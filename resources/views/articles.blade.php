eccd1210fb5802055d4232538d5aa959f258edc2
CBA_Santo <santo@cybridge.jp>
2015-09-28 13:24:50 +0900

    upload composer

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,4 +1,20 @@
-@foreach($articles as $article)
-	<p>Name: [{{$article->name}}]</p>
-	<p>Author: [{{$article->author}}]</p>
-@endforeach
+	@foreach($articles as $article)
+		<p>Name: [{{$article->name}}]</p>
+		<p>Author: [{{$article->author}}]</p>
+	@endforeach
+
+	<hr>
+
+	{!! Form::open(['url' => 'articles']) !!}
+		{!! Form::label('name','Name:') !!}
+		{!! Form::text('name') !!} <br />
+
+		{!! Form::label('author','Author:') !!}
+		{!! Form::text('author') !!} </br>
+
+		{!! Form::submit('Them moi')!!}
+	{!! Form::close() !!}
+
+	<hr>
+
+	
