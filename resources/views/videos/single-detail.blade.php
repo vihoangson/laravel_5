861c82acfd708962a73e500141688e7eee4a46e5
vihoangson <vihoangson@gmail.com>
2015-12-24 23:14:06 +0700

    commit

--- resources/views/videos/single-detail.blade.php
+++ resources/views/videos/single-detail.blade.php
@@ -13,8 +13,8 @@
                             <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{$rs->videos_url}}"></iframe>
                         </div>
                         <div class="row post_title_n_view">
-                            <h2 class="col-sm-8 post_title">Duis aute irure dolor in adipsicing elit perspiciatis omnis</h2>
-                            <h2 class="col-sm-4 view_count">3,45,346 <small>View</small></h2>
+                            <h2 class="col-sm-8 post_title">{{($rs->videos_title)}}</h2>
+                            <h2 class="col-sm-4 view_count">{{number_format($rs->videos_viewcount)}}<small>View</small></h2>
                         </div>
                         <div class="media bio_section">
                             <div class="media-left about_social">
@@ -64,10 +64,7 @@
                                 </div>
                             </div>
                             <div class="media-body author_desc_by_author">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                                <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-                                <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                                {{$rs->videos_note}}
                             </div>
                         </div>
                     </div>
