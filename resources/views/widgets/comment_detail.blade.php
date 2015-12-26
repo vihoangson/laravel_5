aacf659afa99f45498aae7d25b6f5ae46c0fcca1
vihoangson <vihoangson@gmail.com>
2015-12-26 09:06:28 +0700

    commit

---
+++ resources/views/widgets/comment_detail.blade.php
@@ -0,0 +1,55 @@
+                    <div class="row m0 comments">
+                        <h5 class="comment_count">37 Comments:</h5>
+                        <div class="media comment">
+                            <div class="media-left"><a href="#"><img src="<?= THEME_PATH; ?>images/follow/4.jpg" alt="" class="img-circle"></a></div>
+                            <div class="media-body">
+                                <div class="comment_header">
+                                    <h5>
+                                        <a href="#" class="author_name">Lisa Zing Lung Chung</a>
+                                        <span class="time_ago">10 minutes ago</span>
+                                        <a href="#" class="reply_link">Reply</a>
+                                    </h5>                                    
+                                </div>
+                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciat unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                                <div class="media comment comment_reply">
+                                    <div class="media-left"><a href="#"><img src="<?= THEME_PATH; ?>images/follow/4.jpg" alt="" class="img-circle"></a></div>
+                                    <div class="media-body">
+                                        <div class="comment_header">
+                                            <h5>
+                                                <a href="#" class="author_name">Lisa Zing Lung Chung</a>
+                                                <span class="time_ago">10 minutes ago</span>
+                                                <a href="#" class="reply_link">Reply</a>
+                                            </h5>                                    
+                                        </div>
+                                        <p>Sed ut perspiciat unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                                    </div>
+                                </div> <!--Comment Reply-->
+                            </div>
+                        </div> <!--Comment-->
+                        <div class="media comment">
+                            <div class="media-left"><a href="#"><img src="<?= THEME_PATH; ?>images/follow/4.jpg" alt="" class="img-circle"></a></div>
+                            <div class="media-body">
+                                <div class="comment_header">
+                                    <h5>
+                                        <a href="#" class="author_name">Lisa Zing Lung Chung</a>
+                                        <span class="time_ago">10 minutes ago</span>
+                                        <a href="#" class="reply_link">Reply</a>
+                                    </h5>                                    
+                                </div>
+                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciat unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                                <div class="media comment comment_reply">
+                                    <div class="media-left"><a href="#"><img src="<?= THEME_PATH; ?>images/follow/4.jpg" alt="" class="img-circle"></a></div>
+                                    <div class="media-body">
+                                        <div class="comment_header">
+                                            <h5>
+                                                <a href="#" class="author_name">Lisa Zing Lung Chung</a>
+                                                <span class="time_ago">10 minutes ago</span>
+                                                <a href="#" class="reply_link">Reply</a>
+                                            </h5>                                    
+                                        </div>
+                                        <p>Sed ut perspiciat unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                                    </div>
+                                </div> <!--Comment Reply-->
+                            </div>
+                        </div> <!--Comment-->
+                    </div>
